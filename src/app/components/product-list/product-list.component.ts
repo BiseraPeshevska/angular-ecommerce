4f0f5cd213872142794ddb444a985cd6db0ab37d
Bisera Peshevska <pesevskab1997@gmail.com>
2021-07-10 15:41:07 +0200

    commit on master

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-grid.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[];
+  currentCategoryId: number;
+  searchMode: boolean;
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
+  }
+
+  listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
+    }
+  }
+
+  handleSearchProducts() {
+  
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+
+  }
+
+  handleListProducts(){
+    // check if id parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the id param string. convert string to a number using the "+" symbol
+      // @ts-ignore: Object is possibly 'null'.
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else {
+      //if not category id is available set default category id 1
+      this.currentCategoryId = 1;
+    }
+
+    //  get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+}
