5fcb998af0d46075eaf14453c018aba100e5c3cf
Bisera Peshevska <pesevskab1997@gmail.com>
2021-07-13 20:12:33 +0200

    -- added 'login logout' feature

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa1874psuC0BPUXX5d7',
+        issuer:'https://dev-14662575.okta.com/oauth2/default',
+        redirectUri:'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
